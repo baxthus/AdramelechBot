ff57f9d2bb1aba3fb38823307272bf1e08c97b59
Abysmal <abysmal@ttm.sh>
2023-04-07 21:46:08 -0300

    feat(ping): add buttons to ping command
    add velocity and author buttons to the ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,16 +1,31 @@
 import Command from '@interfaces/Command';
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder } from 'discord.js';
 import { embedColor } from '@config';
+import { ButtonStyle } from 'discord.js';
+import ButtonID from '@interfaces/ButtonID';
 
 const ping: Command = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(intr) {
+        const buttons = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel('Author')
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://abysmal.eu.org'),
+                new ButtonBuilder()
+                    .setLabel('Velocity')
+                    .setStyle(ButtonStyle.Primary)
+                    .setCustomId(JSON.stringify({ file: 'speedTest' } as ButtonID))
+            );
+
         await intr.reply({
             embeds: [
                 new EmbedBuilder().setColor(embedColor).setTitle('Pong!'),
             ],
+            components: [buttons],
         });
     },
 };
